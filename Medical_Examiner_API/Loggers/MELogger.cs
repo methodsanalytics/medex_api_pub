c182ff5ddcf285a1af9de69341996d709ea6fb1c
DavidJohnParsons <47145770+DavidJohnParsons@users.noreply.github.com>
2019-02-23 14:49:11 +0000

    use  /// <inheritdoc />

--- Medical_Examiner_API/Loggers/MELogger.cs
+++ Medical_Examiner_API/Loggers/MELogger.cs
@@ -22,17 +22,7 @@ namespace Medical_Examiner_API.Loggers
             _mEloggerPersistence = mEloggerPersistence;
         }
 
-        /// <summary>
-        /// Create log object from parameters and submit to logging destination
-        /// </summary>
-        /// <param name="userName">user name</param>
-        /// <param name="userAuthenticationType">user authentication type</param>
-        /// <param name="userIsAuthenticated">user authentication status</param>
-        /// <param name="controllerName">name of controller called</param>
-        /// <param name="controllerMethod">method of caller called</param>
-        /// <param name="parameters">list of parameters passed to method</param>
-        /// <param name="remoteIP">IP address of client</param>
-        /// <param name="timeStamp">timestamp when method called</param>
+        /// <inheritdoc />
         public async void Log(string userName, string userAuthenticationType, bool userIsAuthenticated,
             string controllerName, string controllerMethod, IList<string> parameters, string remoteIP,
             DateTime timeStamp)
--- Medical_Examiner_API/Loggers/MELoggerMocker.cs
+++ Medical_Examiner_API/Loggers/MELoggerMocker.cs
@@ -13,17 +13,7 @@ namespace Medical_Examiner_API.Loggers
         /// </summary>
         public LogMessageActionDefault LogEntry { get; private set; }
 
-        /// <summary>
-        /// Set LogEntry property based on parameters
-        /// </summary>
-        /// <param name="userName">user name</param>
-        /// <param name="userAuthenticationType">user authentication type</param>
-        /// <param name="userIsAuthenticated">user authentication status</param>
-        /// <param name="controllerName">name of controller called</param>
-        /// <param name="controllerMethod">method of caller called</param>
-        /// <param name="parameters">list of parameters passed to method</param>
-        /// <param name="remoteIP">IP address of client</param>
-        /// <param name="timeStamp">timestamp when method called</param>
+        /// <inheritdoc />
         public void Log(string userName, string userAuthenticationType, bool userIsAuthenticated, string controllerName,
             string controllerMethod, IList<string> parameters, string remoteIP, DateTime timeStamp)
         {
