2c2f68622ab4e067f415dd1b8527100bf7174732
DanukaH <48524567+DanukaH@users.noreply.github.com>
2019-04-09 09:28:49 +0100

    Initial Check in for Case Outcome Dto

---
+++ MedicalExaminer.Models/CaseOutcomes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalExaminer.Models
+{
+    public class CaseOutcomes
+    {
+        /// <summary>
+        /// Determine whether MCCD is Issued (Check box)
+        /// </summary>
+        public bool MccdIssued { get; set; }
+
+        /// <summary>
+        /// Determine whether the Cremation Form is Completed (Yes/No Radio Button Set)
+        /// </summary>
+        public bool CremationFormCompleted { get; set; }
+
+
+    }
+}
