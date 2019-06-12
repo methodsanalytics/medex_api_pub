cdf4a03dbf0164b2e98d9ddff10565380c026e5d
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-12 16:07:53 +0100

    Comments.

--- MedicalExaminer.Common/Queries/User/UserUpdateQuery.cs
+++ MedicalExaminer.Common/Queries/User/UserUpdateQuery.cs
@@ -4,8 +4,16 @@ using MedicalExaminer.Models;
 
 namespace MedicalExaminer.Common.Queries.User
 {
+    /// <summary>
+    /// User Update Query.
+    /// </summary>
     public class UserUpdateQuery : IQuery<MeUser>
     {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UserUpdateQuery"/>.
+        /// </summary>
+        /// <param name="userToUpdate">User to update.</param>
+        /// <param name="currentUser">Current user.</param>
         public UserUpdateQuery(MeUser userToUpdate, MeUser currentUser)
         {
             CurrentUser = currentUser;
@@ -21,12 +29,24 @@ namespace MedicalExaminer.Common.Queries.User
                 : Enumerable.Empty<MEUserPermission>();
         }
 
+        /// <summary>
+        /// Current User.
+        /// </summary>
         public MeUser CurrentUser { get; }
 
+        /// <summary>
+        /// User Id.
+        /// </summary>
         public string UserId { get; }
 
+        /// <summary>
+        /// Email.
+        /// </summary>
         public string Email { get; }
 
+        /// <summary>
+        /// Permissions.
+        /// </summary>
         public IEnumerable<MEUserPermission> Permissions { get; }
     }
 }
