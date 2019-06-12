4775cf23ea641bd2c5c7045216c2363ea0098c47
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-12 16:06:31 +0100

    Comments.

--- MedicalExaminer.Common/Queries/Examination/ExaminationsRetrievalQuery.cs
+++ MedicalExaminer.Common/Queries/Examination/ExaminationsRetrievalQuery.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using MedicalExaminer.Models;
 using MedicalExaminer.Models.Enums;
 
 namespace MedicalExaminer.Common.Queries.Examination
 {
+    /// <summary>
+    /// Examinations Retrieval Query.
+    /// </summary>
     public class ExaminationsRetrievalQuery : IQuery<IEnumerable<Models.Examination>>, IQuery<ExaminationsOverview>
     {
-        public IEnumerable<string> PermissedLocations;
-
-        public readonly CaseStatus? FilterCaseStatus;
-        public readonly string FilterLocationId;
-        public readonly ExaminationsOrderBy? FilterOrderBy;
-        public readonly int FilterPageNumber;
-        public readonly int FilterPageSize;
-        public readonly string FilterUserId;
-        public readonly bool FilterOpenCases;
-
+        /// <summary>
+        /// Initialise a new instance of <see cref="ExaminationsRetrievalQuery"/>.
+        /// </summary>
+        /// <param name="permissedLocations">Permissed locations.</param>
+        /// <param name="filterCaseStatus">case status filter</param>
+        /// <param name="filterLocationId">location id filter</param>
+        /// <param name="filterOrderBy">order by</param>
+        /// <param name="filterPageNumber">page number</param>
+        /// <param name="filterPageSize">page size</param>
+        /// <param name="filterUserId">user id filter</param>
+        /// <param name="filterOpenCases">open cases filter</param>
         public ExaminationsRetrievalQuery(
             IEnumerable<string> permissedLocations,
             CaseStatus? filterCaseStatus,
@@ -36,5 +39,45 @@ namespace MedicalExaminer.Common.Queries.Examination
             FilterUserId = filterUserId;
             FilterOpenCases = filterOpenCases;
         }
+
+        /// <summary>
+        /// Permissed Locations.
+        /// </summary>
+        public IEnumerable<string> PermissedLocations { get; }
+
+        /// <summary>
+        /// Filter Case Status.
+        /// </summary>
+        public CaseStatus? FilterCaseStatus { get; }
+
+        /// <summary>
+        /// Filter Location Id.
+        /// </summary>
+        public string FilterLocationId { get; }
+
+        /// <summary>
+        /// Filter Order By.
+        /// </summary>
+        public ExaminationsOrderBy? FilterOrderBy { get; }
+
+        /// <summary>
+        /// Filter Page Number.
+        /// </summary>
+        public int FilterPageNumber { get; }
+
+        /// <summary>
+        /// Filter Page Size.
+        /// </summary>
+        public int FilterPageSize { get; }
+
+        /// <summary>
+        /// Filter User Id.
+        /// </summary>
+        public string FilterUserId { get; }
+
+        /// <summary>
+        /// Filter Open Cases.
+        /// </summary>
+        public bool FilterOpenCases { get; }
     }
 }
