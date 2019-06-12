88f5af5c12bd7616f64d3bc0162c65f840866382
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-12 15:13:44 +0100

    Comments.

--- MedicalExaminer.Models/IEvent.cs
+++ MedicalExaminer.Models/IEvent.cs
@@ -1,22 +1,46 @@
-﻿using MedicalExaminer.Models.Enums;
-using System;
+﻿using System;
+using MedicalExaminer.Models.Enums;
 
 namespace MedicalExaminer.Models
 {
+    /// <summary>
+    /// Event Interface.
+    /// </summary>
     public interface IEvent
     {
+        /// <summary>
+        /// Event Type.
+        /// </summary>
         EventType EventType { get; }
 
+        /// <summary>
+        /// Event Id.
+        /// </summary>
         string EventId { get; set; }
 
+        /// <summary>
+        /// Is Final.
+        /// </summary>
         bool IsFinal { get; }
 
+        /// <summary>
+        /// User Id.
+        /// </summary>
         string UserId { get; set; }
 
+        /// <summary>
+        /// User Full Name.
+        /// </summary>
         string UserFullName { get; set; }
 
+        /// <summary>
+        /// User Role.
+        /// </summary>
         string UsersRole { get; set; }
 
+        /// <summary>
+        /// Created.
+        /// </summary>
         DateTime? Created { get; set; }
     }
 }
