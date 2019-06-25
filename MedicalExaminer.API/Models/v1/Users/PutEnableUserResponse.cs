7496163911aaf33f4aaa58b1128f4819a5fe3bc5
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-25 16:20:50 +0100

    Response.

---
+++ MedicalExaminer.API/Models/v1/Users/PutEnableUserResponse.cs
@@ -0,0 +1,12 @@
+﻿using MedicalExaminer.Models.Enums;
+
+namespace MedicalExaminer.API.Models.v1.Users
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Response for Suspend/Enable User.
+    /// </summary>
+    public class PutEnableUserResponse : ResponseBase
+    {
+    }
+}
