94155da4af6fac978c96ddb0ab9e4697d54ea5da
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-25 16:19:53 +0100

    New user enable service and query.

---
+++ MedicalExaminer.Common/Queries/User/UserEnableQuery.cs
@@ -0,0 +1,41 @@
+﻿using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Queries.User
+{
+    /// <summary>
+    /// User Enable  Query.
+    /// </summary>
+    public class UserEnableQuery : IQuery<MeUser>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UserEnableQuery"/>.
+        /// </summary>
+        /// <param name="userId">The ID of the user who to enable or disable</param>
+        /// <param name="enableUser">True to enable. False to disable.</param>
+        /// <param name="currentUser">The user performing the change.</param>
+        public UserEnableQuery(string userId, bool enableUser, MeUser currentUser)
+        {
+            CurrentUser = currentUser;
+            UserId = userId;
+            EnableUser = enableUser;
+        }
+
+        /// <summary>
+        /// Current User
+        /// </summary>
+        /// <remarks>The user making the change.</remarks>
+        public MeUser CurrentUser { get; }
+
+        /// <summary>
+        /// User Id
+        /// </summary>
+        /// <remarks>The id of the user to change.</remarks>
+        public string UserId { get; }
+
+        /// <summary>
+        /// Enable User.
+        /// </summary>
+        /// <remarks>True to enable, False to disable</remarks>
+        public bool EnableUser { get; }
+    }
+}
---
+++ MedicalExaminer.Common/Services/User/UserEnableService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.User;
+
+namespace MedicalExaminer.Common.Services.User
+{
+    /// <summary>
+    /// User Enable Service.
+    /// </summary>
+    public class UserEnableService : QueryHandler<UserEnableQuery, Models.MeUser>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UserEnableService"/>.
+        /// </summary>
+        /// <param name="databaseAccess">Database Access.</param>
+        /// <param name="connectionSettings">User Connection Settings.</param>
+        public UserEnableService(IDatabaseAccess databaseAccess, IUserConnectionSettings connectionSettings)
+            : base(databaseAccess, connectionSettings)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override async Task<Models.MeUser> Handle(UserEnableQuery param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(nameof(param));
+            }
+
+            var userToUpdate = await GetItemByIdAsync(param.UserId);
+
+            if (userToUpdate == null)
+            {
+                throw new InvalidOperationException($"User with id `{param.UserId}` not found.");
+            }
+
+            userToUpdate.Active = param.EnableUser;
+
+            userToUpdate.LastModifiedBy = param.CurrentUser.UserId;
+            userToUpdate.ModifiedAt = DateTime.Now;
+
+            var result = await UpdateItemAsync(userToUpdate);
+            return result;
+        }
+    }
+}
