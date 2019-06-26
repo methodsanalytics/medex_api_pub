0763be3ea53c875a4e255553e0c3c83fe1cd9535
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-26 12:32:09 +0100

    Delete query and service.

---
+++ MedicalExaminer.Common/Queries/User/UserDeleteQuery.cs
@@ -0,0 +1,27 @@
+﻿using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Queries.User
+{
+    /// <summary>
+    /// User Delete Query.
+    /// </summary>
+    public class UserDeleteQuery : AuthenticatedQuery<MeUser>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UserDeleteQuery"/>
+        /// </summary>
+        /// <param name="userId">The user identifier to delete.</param>
+        /// <param name="authenticatedUser">The authenticated user performing the query.</param>
+        public UserDeleteQuery(string userId, MeUser authenticatedUser)
+            : base(authenticatedUser)
+        {
+            UserId = userId;
+        }
+
+        /// <summary>
+        /// User Id
+        /// </summary>
+        /// <remarks>The ID of the user who is to be deleted.</remarks>
+        public string UserId { get; }
+    }
+}
---
+++ MedicalExaminer.Common/Services/User/UserDeleteService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using MedicalExaminer.Common.ConnectionSettings;
+using MedicalExaminer.Common.Database;
+using MedicalExaminer.Common.Queries.User;
+
+namespace MedicalExaminer.Common.Services.User
+{
+    /// <summary>
+    /// User Delete Service.
+    /// </summary>
+    /// <remarks>Soft Delete. Marks the user as deleted and updates the modified by.</remarks>
+    public class UserDeleteService : QueryHandler<UserDeleteQuery, Models.MeUser>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="UserUpdateService"/>.
+        /// </summary>
+        /// <param name="databaseAccess">Database Access.</param>
+        /// <param name="connectionSettings">User Connection Settings.</param>
+        public UserDeleteService(IDatabaseAccess databaseAccess, IUserConnectionSettings connectionSettings)
+            : base(databaseAccess, connectionSettings)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override async Task<Models.MeUser> Handle(UserDeleteQuery param)
+        {
+            if (param == null)
+            {
+                throw new ArgumentNullException(nameof(param));
+            }
+
+            var userToUpdate = await GetItemByIdAsync(param.UserId);
+
+            if (userToUpdate == null)
+            {
+                throw new InvalidOperationException($"User with id `{param.UserId}` not found.");
+            }
+
+            userToUpdate.Deleted = true;
+            userToUpdate.LastModifiedBy = param.AuthenticatedUser.UserId;
+            userToUpdate.ModifiedAt = DateTime.UtcNow;
+
+            var result = await UpdateItemAsync(userToUpdate);
+            return result;
+        }
+    }
+}
