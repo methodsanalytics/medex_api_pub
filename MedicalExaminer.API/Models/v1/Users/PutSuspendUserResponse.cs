73c314ccc20ffbf811e61372cce5a7878dfb664e
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-26 12:30:26 +0100

    Refactor enable user to suspend user response.

---
+++ MedicalExaminer.API/Models/v1/Users/PutSuspendUserResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalExaminer.API.Models.v1.Users
+{
+    /// <summary>
+    /// Response for Suspend User.
+    /// </summary>
+    /// <inheritdoc />
+    public class PutSuspendUserResponse : ResponseBase
+    {
+    }
+}
