9b79ebd48f612f7503cf99db9b2d28b07c749b09
Christopher Lightfoot <lightfoot256@googlemail.com>
2019-06-26 12:31:06 +0100

    Authenticated query that has current/auethenticated user as part of it.

---
+++ MedicalExaminer.Common/Queries/AuthenticatedQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MedicalExaminer.Models;
+
+namespace MedicalExaminer.Common.Queries
+{
+    /// <summary>
+    /// Authenticated Query.
+    /// </summary>
+    /// <typeparam name="T">Type of query.</typeparam>
+    public class AuthenticatedQuery<T> : IQuery<T>
+    {
+        /// <summary>
+        /// Initialise a new instance of <see cref="AuthenticatedQuery{T}"/>.
+        /// </summary>
+        /// <param name="authenticatedUser">The authenticated user.</param>
+        public AuthenticatedQuery(MeUser authenticatedUser)
+        {
+            AuthenticatedUser = authenticatedUser;
+        }
+
+        /// <summary>
+        /// Authenticated User.
+        /// </summary>
+        public MeUser AuthenticatedUser { get; }
+    }
+}
