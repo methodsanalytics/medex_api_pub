4525962128280bfcc6542521b9950ce014a0ee28
mark <sharkeymark@gmail.com>
2019-08-13 21:34:43 +0100

    quick fixes
    before the unit tests!

--- MedicalExaminer.Migration/MigrationProcessor.cs
+++ MedicalExaminer.Migration/MigrationProcessor.cs
@@ -18,28 +18,33 @@ namespace MedicalExaminer.Migration
             
             foreach (var record in recordsToMigrate)
             {
-                int version = 0;
+                bool updated = false;
+                long version = 0;
                 IMigrationDefinition migrationDefinition = null;
                 var recordAsDictionary = GetDictionary(record);
                 var migratedAsDictionary = new Dictionary<string, object>();
                 if (!recordAsDictionary.ContainsKey("version"))
                 {
-                    migrationDefinition = _migrations[version];
+                    migrationDefinition = _migrations[(int)version];
                     migratedAsDictionary = MigrateToVersion(recordAsDictionary, migrationDefinition);
+                    updated = true;
                 }
 
-                version = (int)migratedAsDictionary["version"];
+                version = (long)recordAsDictionary["version"];
 
                 while (version < migrateToVersion)
                 {
                     version = version + 1;
-                    migrationDefinition = _migrations[version];
+                    migrationDefinition = _migrations[(int)version];
                     migratedAsDictionary = MigrateToVersion(migratedAsDictionary, migrationDefinition);
+                    updated = true;
                 }
-
-                var migratedAsJson = JsonConvert.SerializeObject(migratedAsDictionary);
-                var migratedObject = JsonConvert.DeserializeObject<T>(migratedAsJson);
-                results.Add(migratedObject);
+                if (updated)
+                {
+                    var migratedAsJson = JsonConvert.SerializeObject(migratedAsDictionary);
+                    var migratedObject = JsonConvert.DeserializeObject<T>(migratedAsJson);
+                    results.Add(migratedObject);
+                }
             }
 
             return results;
